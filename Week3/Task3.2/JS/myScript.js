ff7b2312104aa22de1327763c5c1932b5045a95c
P Tam <Ongput2000@gmail.com>
2019-07-20 20:58:27 +0700

    last commit

--- Week3/Task3.2/JS/myScript.js
+++ Week3/Task3.2/JS/myScript.js
@@ -46,9 +46,29 @@ function Tinh(pstuoi)
 
 function getTime()
 {
-    
-    var birth = document.getElementById("formage3").value;
-    Tinh(birth);
     var name = document.getElementById("formage0").value;
     document.getElementById("Name").innerHTML = name;
+
+    //-----------------
+    var birth = new Date(document.getElementById("formage3").value); 
+    var pre = new Date(); 
+    span = pre - birth;
+    var displayYear = Math.floor( span / 1000 / 3600 / 24 / 365);
+    span -= displayYear*1000*3600*24*365;
+    var displayMonth = Math.floor(span /1000/3600/24/30);
+    span -= displayMonth*24*3600*1000*30;
+    var displayDay = Math.floor(span/1000/3600/24);
+    span -= displayDay*24*3600*1000;
+    var displayHours = Math.floor(span /1000/3600);
+    span -= displayHours*3600*1000;
+    var displayMinutes = Math.floor(span/1000/60);
+    span -= displayMinutes*60*1000;
+    var displaySecond = Math.floor(span/1000);
+    document.getElementById("tuoi").innerHTML = "Bạn đang " + displayYear   + " tuổi và bạn đã sống được " + displayYear + " năm "
+    + displayMonth + " tháng " + displayDay + " ngày " + displayHours + " giờ " + displayMinutes + " phút " + displaySecond + " giây "; 
+}
+
+function loadlive()
+{
+    var t = setInterval(getTime,1000);
 }
